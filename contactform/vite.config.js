c935c3cb86a0733994bf585b2349280d34ff627b
Effy96 <akinyieffy07@gmail.com>
2025-02-14 07:45:21 +0300

    update

--- contactform/vite.config.js
+++ contactform/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Ensure Vite knows where the project root is
+  build: {
+    outDir: 'dist', // Ensure the build output goes to the correct folder
+  },
 })
